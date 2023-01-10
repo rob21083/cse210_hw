d884993e8ba20df43348c122c4f0c611338755c6
rob21083 <rob21083@byui.edu>
2023-01-10 15:38:25 -0700

    Prep work 5 functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        displayWelcomeMessage();
+
+        string inputName = userName();
+        int inputNumber = promptUserNumber();
+
+        int squared = square(inputNumber);
+
+        results(inputName, squared);
+    }
+
+    static void displayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string userName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int promptUserNumber()
+    {
+      Console.Write("Please enter your favorite number: ");  
+      int number = int.Parse(Console.ReadLine());
+      return number;
+    }
+
+    static int square(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void results (string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
