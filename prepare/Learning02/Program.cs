dd5b5148fccf0a8c149d27c1d8e1a5dfa8475738
rob21083 <rob21083@byui.edu>
2023-01-25 20:06:36 -0700

    Testing

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        job job1 = new job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+
+        job job2 = new job();
+        job2._company = "Apple";
     }
 }
